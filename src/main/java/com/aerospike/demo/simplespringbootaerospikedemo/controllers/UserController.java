86f99819fe5219eb571251e9d2b7bb64227efeb6
roimenashe <roimenashe5@gmail.com>
2021-02-14 16:58:25 +0200

    Support create,read and delete operations.

--- src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/controllers/UserController.java
+++ src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/controllers/UserController.java
@@ -5,14 +5,26 @@ import com.aerospike.demo.simplespringbootaerospikedemo.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @AllArgsConstructor
 public class UserController {
 
     UserService userService;
 
-    @PostMapping("/add")
-    public void addUserTest(@RequestBody User user) {
+    @GetMapping("/users/{id}")
+    public Optional<User> readUserById(@PathVariable("id") Integer id) {
+        return userService.readUserById(id);
+    }
+
+    @PostMapping("/users")
+    public void addUser(@RequestBody User user) {
         userService.addUser(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUserById(@PathVariable("id") Integer id) {
+        userService.removeUserById(id);
     }
 }
--- src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/services/UserService.java
+++ src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/services/UserService.java
@@ -5,13 +5,23 @@ import com.aerospike.demo.simplespringbootaerospikedemo.repositories.AerospikeUs
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class UserService {
 
     AerospikeUserRepository aerospikeUserRepository;
 
+    public Optional<User> readUserById(int id) {
+        return aerospikeUserRepository.findById(id);
+    }
+
     public void addUser(User user) {
         aerospikeUserRepository.save(user);
+    }
+
+    public void removeUserById(int id) {
+        aerospikeUserRepository.deleteById(id);
     }
 }
