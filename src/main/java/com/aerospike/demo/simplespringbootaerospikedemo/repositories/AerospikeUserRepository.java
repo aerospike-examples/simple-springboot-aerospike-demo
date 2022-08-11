17f19ce0c981b4327b63520fed41a8ce99b0c71b
roimenashe <roimenashe5@gmail.com>
2022-08-11 20:22:32 +0300

    Change repository ID from Object to Integer since User @id is an integer.

--- src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/repositories/AerospikeUserRepository.java
+++ src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/repositories/AerospikeUserRepository.java
@@ -3,5 +3,5 @@ package com.aerospike.demo.simplespringbootaerospikedemo.repositories;
 import com.aerospike.demo.simplespringbootaerospikedemo.objects.User;
 import org.springframework.data.aerospike.repository.AerospikeRepository;
 
-public interface AerospikeUserRepository extends AerospikeRepository<User, Object> {
+public interface AerospikeUserRepository extends AerospikeRepository<User, Integer> {
 }
