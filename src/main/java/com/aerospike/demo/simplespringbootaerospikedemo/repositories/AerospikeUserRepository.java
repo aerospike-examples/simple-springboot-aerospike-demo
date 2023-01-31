3ec1ac61ea6a40ec70cce559382258f9b518b7f2
agr <agrosland@aerospike.com>
2023-01-31 16:14:11 +0200

    dependencies update (Spring Boot 3, spring-data-aerospike 4.0.0)

--- src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/repositories/AerospikeUserRepository.java
+++ src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/repositories/AerospikeUserRepository.java
@@ -2,6 +2,7 @@ package com.aerospike.demo.simplespringbootaerospikedemo.repositories;
 
 import com.aerospike.demo.simplespringbootaerospikedemo.objects.User;
 import org.springframework.data.aerospike.repository.AerospikeRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface AerospikeUserRepository extends AerospikeRepository<User, Integer> {
+public interface AerospikeUserRepository extends AerospikeRepository<User, Integer>, CrudRepository<User, Integer> {
 }
